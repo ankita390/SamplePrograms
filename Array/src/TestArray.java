ae6adeb5627b18087b5c5e9c019e3639a274e40d
anup.kumar <anup.kumar@meritnation.com>
2018-04-17 15:26:36 +0530

    Added Logout Page Test and code in exixting scripts

---
+++ Array/src/TestArray.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+
+import org.testng.annotations.Test;
+
+public class TestArray {
+	@Test
+	
+
+	public void hello() {
+		
+		        
+	}
+		
+	/*	int i,j, k=1;
+		for (i=0;i<5;i++){
+			for(j=0;j<=i;j++){
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+		for (i=0;i<5;i++){
+			for(j=0;j<k;j++){
+				System.out.print("* ");
+			}
+			k=k+2;
+			System.out.println();
+		}
+		for (i=0;i<5;i++){
+			for(j=0;j<k+7;j++){
+				System.out.print("");
+			}
+			k=k-(-5);
+			System.out.println("* ");
+		}
+		
+		
+		int[] x=new int[3];
+		System.out.println(x);
+		System.out.print(x[0]);
+		System.out.print(x[1]);
+		System.out.print(x[2]);
+	}*/
+
+}
---
+++ MeritnationAutomation/src/com/applect/meritnation/generic/Locators.java
@@ -0,0 +1,9 @@
+package com.applect.meritnation.generic;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Locators {
+	
+	
+}
